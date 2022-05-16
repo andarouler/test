0d5cc94a043d3647fdfa0399aeda8fdf25f1cc85
studschian2887 <andi.andi8818@gmail.com>
2022-05-16 16:15:19 +0200

    add h2

--- src/dto/content.dto.ts
+++ src/dto/content.dto.ts
@@ -2,7 +2,7 @@ export class ContentDTO{
     contentId: number;
     //interviewId: Interview;
     headline1: string = "Wilkommen zu deinem Videointerview!";
-    headline2: string = "";
+    headline2: string = "Hier kannst du deine Kamera testen.";
     introText: string;
     buttonText: string;
     duration: number = 180;
